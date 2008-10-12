102f35053ba0b224e7973fa27d6907fe5b123dbf
robert.schuppenies <robert.schuppenies@9e6fa867-6b55-0410-95a4-a37b91626726>
2008-10-12 19:03:01 +0000

    Fixed typo.

--- pympler/muppy/__init__.py
+++ pympler/muppy/__init__.py
@@ -1,4 +1,4 @@
-"""Tool set for measuring the memory usage of Python applications.
+"""Toolset for measuring the memory usage of Python applications.
 
 Be aware that muppy will call gc.collect() whenever the object state is
 gathered to remove reference cycles.
