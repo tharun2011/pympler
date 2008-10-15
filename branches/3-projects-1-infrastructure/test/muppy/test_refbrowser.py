cd89c43698129393abc7793af6193e542e2dc367
robert.schuppenies <robert.schuppenies@9e6fa867-6b55-0410-95a4-a37b91626726>
2008-10-15 13:48:43 +0000

    Limit tree size. This reduces the computation time while still being sufficient for testing.

--- branches/3-projects-1-infrastructure/test/muppy/test_refbrowser.py
+++ branches/3-projects-1-infrastructure/test/muppy/test_refbrowser.py
@@ -79,9 +79,10 @@ class TreeTest(unittest.TestCase):
         # test if the str_func is applied correctly
         expected = 'the quick brown fox'
         def foo(o): return expected
-        res = refbrowser.RefBrowser(root, str_func=foo).get_tree()
+        res = refbrowser.RefBrowser(root, str_func=foo, maxdepth=2).get_tree()
         self.assert_(str(res) == expected)
-        res = refbrowser.RefBrowser(root, str_func=foo, repeat=True).get_tree()
+        res = refbrowser.RefBrowser(root, str_func=foo, repeat=True,\
+                                    maxdepth=2).get_tree()
         self.assert_(str(res) == expected)
 
         
